154104dd80d71a78d5288b6e76707596d728fc0f
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-08-02 19:10:38 +0700

    Major Update

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module Git-Secret
+module github.com/daffainfo/Git-Secret
 
 go 1.16
+
+replace github.com/daffainfo/Git-Secret => /Git-Secret
--- main.go
+++ main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"bufio"
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"regexp"
 	"strings"
 )
 
@@ -15,6 +18,7 @@ type Data struct {
 	Tree      []Tree `json:"tree"`
 	Truncated bool   `json:"truncated"`
 }
+
 type Tree struct {
 	Path string `json:"path"`
 	Mode string `json:"mode"`
@@ -22,6 +26,7 @@ type Tree struct {
 	Sha  string `json:"sha"`
 	URL  string `json:"url"`
 }
+
 type ContentFile struct {
 	Name        string `json:"name"`
 	Path        string `json:"path"`
@@ -46,9 +51,21 @@ type Branch []struct {
 	Commit    Commit `json:"commit"`
 	Protected bool   `json:"protected"`
 }
+
 type Commit struct {
 	Sha string `json:"sha"`
 	URL string `json:"url"`
+}
+
+func show_banner() {
+	fmt.Println(`
+____ _ ___    ____ ____ ____ ____ ____ ___ 
+| __ |  |  __ [__  |___ |    |__/ |___  |  
+|__] |  |     ___] |___ |___ |  \ |___  |  
+
+Author: Muhammad Daffa
+Version: 1.0								   
+	`)
 }
 
 var (
@@ -90,13 +107,73 @@ func getListFile(url string) {
 	}
 }
 
-func main() {
-	var url string
-	var choose_branch int
+func google_api(contents string) {
+	re := regexp.MustCompile(`AIza[0-9A-Za-z-_]{35}`)
+	if re.MatchString(contents) {
+		res1 := re.FindAllString(contents, 2)
+		fmt.Println("[+] Google API Key: ", res1)
+	}
+}
 
+func twitter_secret(contents string) {
+	re := regexp.MustCompile(`(?i)twitter(.{0,20})?[0-9a-z]{35,44}`)
+	if re.MatchString(contents) {
+		res1 := re.FindAllString(contents, 2)
+		fmt.Println("[+] Twitter Secret: ", res1)
+	}
+}
+
+func twilio_api(contents string) {
+	re := regexp.MustCompile(`(?i)twilio(.{0,20})?SK[0-9a-f]{32}`)
+	if re.MatchString(contents) {
+		res1 := re.FindAllString(contents, 2)
+		fmt.Println("[+] Twilio API: ", res1)
+	}
+}
+
+func stripe_api(contents string) {
+	re := regexp.MustCompile(`(?i)stripe(.{0,20})?[sr]k_live_[0-9a-zA-Z]{24}`)
+	if re.MatchString(contents) {
+		res1 := re.FindAllString(contents, 2)
+		fmt.Println("[+] Stripe API: ", res1)
+	}
+}
+
+func dork_file(path string, contents string) {
+	file, err := os.Open(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		// fmt.Println(scanner.Text())
+		re := regexp.MustCompile(scanner.Text())
+		if re.MatchString(contents) {
+			res1 := re.FindAllString(contents, 2)
+			fmt.Println("[+] FOUND: ", res1)
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	var url, dorks string
+	var choose_branch int
+	show_banner()
+
+	fmt.Println("Input URL")
 	fmt.Scanln(&url)
+
+	fmt.Println("\nInput path file contain dorks (Leave it blank if you dont have it)")
+	fmt.Scanln(&dorks)
 	splitted := strings.Split(url, "/")
 
+	fmt.Println("\nChoose Branches: ")
 	getBranchName("https://api.github.com/repos/" + splitted[3] + "/" + splitted[4] + "/branches")
 
 	fmt.Scanln(&choose_branch)
@@ -111,11 +188,21 @@ func main() {
 			defer resp.Body.Close()
 			body, _ := ioutil.ReadAll(resp.Body)
 
-			fmt.Println(data.Tree[i].Path)
+			fmt.Println("[#] Checking: ", data.Tree[i].Path)
 
-			a.google_api(string(body))
-			a.twitter_secret(string(body))
-			a.twilio_api(string(body))
+			s := strings.Split(data.Tree[i].Path, ".")
+			ext := s[len(s)-1]
+			if ext == "jar" || ext == "jpg" || ext == "jpeg" || ext == "png" || ext == "exe" {
+				continue
+			}
+			if dorks != "" {
+				dork_file(dorks, string(body))
+			}
+
+			google_api(string(body))
+			twitter_secret(string(body))
+			twilio_api(string(body))
+			stripe_api(string(body))
 		}
 	}
 }
