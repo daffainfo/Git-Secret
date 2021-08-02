e733c21a8454b36f4c93ef27509c4efc91cd8950
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-08-02 20:12:27 +0700

    Add color and fix minor bugs

--- main.go
+++ main.go
@@ -58,25 +58,37 @@ type Commit struct {
 }
 
 func show_banner() {
-	fmt.Println(`
+	fmt.Println(Blue(`
 ____ _ ___    ____ ____ ____ ____ ____ ___ 
 | __ |  |  __ [__  |___ |    |__/ |___  |  
 |__] |  |     ___] |___ |___ |  \ |___  |  
 
 Author: Muhammad Daffa
 Version: 1.0								   
-	`)
+	`))
 }
 
 var (
 	branch Branch
 	data   Data
+	Red    = Color("\033[1;31m%s\033[0m")
+	Green  = Color("\033[1;32m%s\033[0m")
+	Blue   = Color("\033[1;34m%s\033[0m")
+	Cyan   = Color("\033[1;36m%s\033[0m")
 )
+
+func Color(colorString string) func(...interface{}) string {
+	sprint := func(args ...interface{}) string {
+		return fmt.Sprintf(colorString,
+			fmt.Sprint(args...))
+	}
+	return sprint
+}
 
 func getBranchName(url string) {
 	req, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(Red(err))
 	}
 	defer req.Body.Close()
 
@@ -84,18 +96,18 @@ func getBranchName(url string) {
 
 	err = json.Unmarshal(body, &branch)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(Red(err))
 	}
 
 	for i := range branch {
-		fmt.Printf("%d. %s\n", i+1, branch[i].Name)
+		fmt.Println(Cyan(i+1, ". ", branch[i].Name))
 	}
 }
 
 func getListFile(url string) {
 	req, err := http.Get(url)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(Red(err))
 	}
 	defer req.Body.Close()
 
@@ -103,7 +115,7 @@ func getListFile(url string) {
 
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(Red(err))
 	}
 }
 
@@ -111,38 +123,46 @@ func google_api(contents string) {
 	re := regexp.MustCompile(`AIza[0-9A-Za-z-_]{35}`)
 	if re.MatchString(contents) {
 		res1 := re.FindAllString(contents, 2)
-		fmt.Println("[+] Google API Key: ", res1)
+		fmt.Println(Green("[+] Google API Key: ", res1))
 	}
 }
 
 func twitter_secret(contents string) {
 	re := regexp.MustCompile(`(?i)twitter(.{0,20})?[0-9a-z]{35,44}`)
 	if re.MatchString(contents) {
-		res1 := re.FindAllString(contents, 2)
-		fmt.Println("[+] Twitter Secret: ", res1)
+		res1 := re.FindAllString(contents, 1)
+		fmt.Println(Green("[+] Twitter Secret: ", res1))
 	}
 }
 
 func twilio_api(contents string) {
 	re := regexp.MustCompile(`(?i)twilio(.{0,20})?SK[0-9a-f]{32}`)
 	if re.MatchString(contents) {
-		res1 := re.FindAllString(contents, 2)
-		fmt.Println("[+] Twilio API: ", res1)
+		res1 := re.FindAllString(contents, 1)
+		fmt.Println(Green("[+] Twilio API: ", res1))
 	}
 }
 
 func stripe_api(contents string) {
 	re := regexp.MustCompile(`(?i)stripe(.{0,20})?[sr]k_live_[0-9a-zA-Z]{24}`)
 	if re.MatchString(contents) {
-		res1 := re.FindAllString(contents, 2)
-		fmt.Println("[+] Stripe API: ", res1)
+		res1 := re.FindAllString(contents, 1)
+		fmt.Println(Green("[+] Stripe API: ", res1))
+	}
+}
+
+func slack_webhook(contents string) {
+	re := regexp.MustCompile(`https://hooks.slack.com/services/T[0-9A-Za-z\\-_]{8}/B[0-9A-Za-z\\-_]{8}/[0-9A-Za-z\\-_]{24}`)
+	if re.MatchString(contents) {
+		res1 := re.FindAllString(contents, 1)
+		fmt.Println(Green("[+] Slack Webhook: ", res1))
 	}
 }
 
 func dork_file(path string, contents string) {
 	file, err := os.Open(path)
 	if err != nil {
-		log.Fatal(err)
+		log.Fatal(Red(err))
 	}
 	defer file.Close()
 
@@ -152,12 +172,12 @@ func dork_file(path string, contents string) {
 		re := regexp.MustCompile(scanner.Text())
 		if re.MatchString(contents) {
 			res1 := re.FindAllString(contents, 2)
-			fmt.Println("[+] FOUND: ", res1)
+			fmt.Println(Green("[+] FOUND WORD: ", res1[0]))
 		}
 	}
 
 	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
+		log.Fatal(Red(err))
 	}
 }
 
@@ -166,35 +186,38 @@ func main() {
 	var choose_branch int
 	show_banner()
 
-	fmt.Println("Input URL")
+	fmt.Println(Blue("Input URL"))
 	fmt.Scanln(&url)
 
-	fmt.Println("\nInput path file contain dorks (Leave it blank if you dont have it)")
+	fmt.Println(Blue("\nInput path file contain dorks (Leave it blank if you dont have it)"))
 	fmt.Scanln(&dorks)
 	splitted := strings.Split(url, "/")
 
-	fmt.Println("\nChoose Branches: ")
+	fmt.Println(Blue("\nChoose Branches: "))
 	getBranchName("https://api.github.com/repos/" + splitted[3] + "/" + splitted[4] + "/branches")
 
 	fmt.Scanln(&choose_branch)
 	getListFile("https://api.github.com/repos/" + splitted[3] + "/" + splitted[4] + "/git/trees/" + branch[choose_branch-1].Name + "?recursive=1")
 
+	fmt.Println(Blue("\nStarting...."))
+
 	for i := range data.Tree {
 		if data.Tree[i].Type == "blob" {
-			resp, err := http.Get("https://raw.githubusercontent.com/" + splitted[3] + "/" + splitted[4] + "/" + branch[choose_branch-1].Name + "/" + data.Tree[i].Path)
-			if err != nil {
-				log.Fatalln(err)
-			}
-			defer resp.Body.Close()
-			body, _ := ioutil.ReadAll(resp.Body)
-
-			fmt.Println("[#] Checking: ", data.Tree[i].Path)
-
 			s := strings.Split(data.Tree[i].Path, ".")
 			ext := s[len(s)-1]
 			if ext == "jar" || ext == "jpg" || ext == "jpeg" || ext == "png" || ext == "exe" {
 				continue
 			}
+
+			resp, err := http.Get("https://raw.githubusercontent.com/" + splitted[3] + "/" + splitted[4] + "/" + branch[choose_branch-1].Name + "/" + data.Tree[i].Path)
+			if err != nil {
+				log.Fatalln(Red(err))
+			}
+			defer resp.Body.Close()
+			body, _ := ioutil.ReadAll(resp.Body)
+
+			fmt.Println(Cyan("[#] Checking: ", data.Tree[i].Path))
+
 			if dorks != "" {
 				dork_file(dorks, string(body))
 			}
@@ -203,6 +226,7 @@ func main() {
 			twitter_secret(string(body))
 			twilio_api(string(body))
 			stripe_api(string(body))
+			slack_webhook(string(body))
 		}
 	}
 }
